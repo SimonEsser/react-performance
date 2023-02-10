76c7a6f9b69a6b71f86b4609ea831e0b5705cfba
Simon Esser <simonesser@Simons-MBP.fritz.box>
2023-02-10 13:24:48 +0100

    completes extra credit 2 of exercise 03

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -21,8 +21,8 @@ function Menu({
           getItemProps={getItemProps}
           item={item}
           index={index}
-          selectedItem={selectedItem}
-          highlightedIndex={highlightedIndex}
+          isSelected={selectedItem?.id === item.id}
+          isHighlighted={highlightedIndex === index}
         >
           {item.name}
         </ListItem>
@@ -37,12 +37,12 @@ function ListItem({
   getItemProps,
   item,
   index,
-  selectedItem,
-  highlightedIndex,
+  isSelected,
+  isHighlighted,
   ...props
 }) {
-  const isSelected = selectedItem?.id === item.id
-  const isHighlighted = highlightedIndex === index
+  // const isSelected = selectedItem?.id === item.id
+  // const isHighlighted = highlightedIndex === index
   return (
     <li
       {...getItemProps({
@@ -58,21 +58,22 @@ function ListItem({
   )
 }
 // 🐨 Memoize the ListItem here using React.memo
-ListItem = React.memo(ListItem, (prevProps, nextProps) => {
-  if (prevProps.getItemProps !== nextProps.getItemProps) return false
-  if (prevProps.item !== nextProps.item) return false
-  if (prevProps.selectedItem !== nextProps.selectedItem) return false
-  if (prevProps.index !== nextProps.index) return false
+ListItem = React.memo(ListItem)
+// ListItem = React.memo(ListItem, (prevProps, nextProps) => {
+//   if (prevProps.getItemProps !== nextProps.getItemProps) return false
+//   if (prevProps.item !== nextProps.item) return false
+//   if (prevProps.selectedItem !== nextProps.selectedItem) return false
+//   if (prevProps.index !== nextProps.index) return false
 
-  if (prevProps.highlightedIndex !== nextProps.highlightedIndex) {
-    const wasPrevHighlighted = prevProps.highlightedIndex === prevProps.index
-    const isNowHighlighted = nextProps.highlightedIndex === nextProps.index
+//   if (prevProps.highlightedIndex !== nextProps.highlightedIndex) {
+//     const wasPrevHighlighted = prevProps.highlightedIndex === prevProps.index
+//     const isNowHighlighted = nextProps.highlightedIndex === nextProps.index
 
-    return wasPrevHighlighted === isNowHighlighted
-  }
+//     return wasPrevHighlighted === isNowHighlighted
+//   }
 
-  return true
-})
+//   return true
+// })
 
 function App() {
   const forceRerender = useForceRerender()
